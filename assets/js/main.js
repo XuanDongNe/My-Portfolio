05771c2d49048c78ac478ce371b7bae56615f234
=Xuan Dong Ne <xuandong1405@gmail.com>
2023-11-11 00:00:53 +0700

    ft:add

--- assets/js/main.js
+++ assets/js/main.js
@@ -181,7 +181,7 @@ $(document).ready(function () {
       $('.tabcontent').hide();
       $(id_tab_content).fadeIn();
     }
-   
+
     return false;
   });
 });
@@ -212,95 +212,168 @@ $('.download-btn').click(function () {
   return false;
 });
 
-// google maps
+// ========================================================================= //
+//  /.GOOGLE MAP
+// ========================================================================= //
 function initMap() {
   // Styles a map in night mode.
-      var map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: 40.674, lng: -73.945},
-          zoom: 12,
-          scrollwheel:  false,
-          navigationControl: false,
-          mapTypeControl: false,
-          scaleControl: false,
-        styles: [
-          {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
-          {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
-          {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
-          {
-            featureType: 'administrative.locality',
-            elementType: 'labels.text.fill',
-            stylers: [{color: '#d59563'}]
-          },
-          {
-            featureType: 'poi',
-            elementType: 'labels.text.fill',
-            stylers: [{color: '#d59563'}]
-          },
-          {
-            featureType: 'poi.park',
-            elementType: 'geometry',
-            stylers: [{color: '#263c3f'}]
-          },
-          {
-            featureType: 'poi.park',
-            elementType: 'labels.text.fill',
-            stylers: [{color: '#6b9a76'}]
-          },
-          {
-            featureType: 'road',
-            elementType: 'geometry',
-            stylers: [{color: '#38414e'}]
-          },
-          {
-            featureType: 'road',
-            elementType: 'geometry.stroke',
-            stylers: [{color: '#212a37'}]
-          },
-          {
-            featureType: 'road',
-            elementType: 'labels.text.fill',
-            stylers: [{color: '#9ca5b3'}]
-          },
-          {
-            featureType: 'road.highway',
-            elementType: 'geometry',
-            stylers: [{color: '#746855'}]
-          },
-          {
-            featureType: 'road.highway',
-            elementType: 'geometry.stroke',
-            stylers: [{color: '#1f2835'}]
-          },
-          {
-            featureType: 'road.highway',
-            elementType: 'labels.text.fill',
-            stylers: [{color: '#f3d19c'}]
-          },
-          {
-            featureType: 'transit',
-            elementType: 'geometry',
-            stylers: [{color: '#2f3948'}]
-          },
-          {
-            featureType: 'transit.station',
-            elementType: 'labels.text.fill',
-            stylers: [{color: '#d59563'}]
-          },
-          {
-            featureType: 'water',
-            elementType: 'geometry',
-            stylers: [{color: '#17263c'}]
-          },
-          {
-            featureType: 'water',
-            elementType: 'labels.text.fill',
-            stylers: [{color: '#515c6d'}]
-          },
-          {
-            featureType: 'water',
-            elementType: 'labels.text.stroke',
-            stylers: [{color: '#17263c'}]
-          }
-        ]
-      });
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: { lat: 40.674, lng: -73.945 },
+    zoom: 12,
+    scrollwheel: false,
+    navigationControl: false,
+    mapTypeControl: false,
+    scaleControl: false,
+    styles: [
+      { elementType: 'geometry', stylers: [{ color: '#242f3e' }] },
+      { elementType: 'labels.text.stroke', stylers: [{ color: '#242f3e' }] },
+      { elementType: 'labels.text.fill', stylers: [{ color: '#746855' }] },
+      {
+        featureType: 'administrative.locality',
+        elementType: 'labels.text.fill',
+        stylers: [{ color: '#d59563' }]
+      },
+      {
+        featureType: 'poi',
+        elementType: 'labels.text.fill',
+        stylers: [{ color: '#d59563' }]
+      },
+      {
+        featureType: 'poi.park',
+        elementType: 'geometry',
+        stylers: [{ color: '#263c3f' }]
+      },
+      {
+        featureType: 'poi.park',
+        elementType: 'labels.text.fill',
+        stylers: [{ color: '#6b9a76' }]
+      },
+      {
+        featureType: 'road',
+        elementType: 'geometry',
+        stylers: [{ color: '#38414e' }]
+      },
+      {
+        featureType: 'road',
+        elementType: 'geometry.stroke',
+        stylers: [{ color: '#212a37' }]
+      },
+      {
+        featureType: 'road',
+        elementType: 'labels.text.fill',
+        stylers: [{ color: '#9ca5b3' }]
+      },
+      {
+        featureType: 'road.highway',
+        elementType: 'geometry',
+        stylers: [{ color: '#746855' }]
+      },
+      {
+        featureType: 'road.highway',
+        elementType: 'geometry.stroke',
+        stylers: [{ color: '#1f2835' }]
+      },
+      {
+        featureType: 'road.highway',
+        elementType: 'labels.text.fill',
+        stylers: [{ color: '#f3d19c' }]
+      },
+      {
+        featureType: 'transit',
+        elementType: 'geometry',
+        stylers: [{ color: '#2f3948' }]
+      },
+      {
+        featureType: 'transit.station',
+        elementType: 'labels.text.fill',
+        stylers: [{ color: '#d59563' }]
+      },
+      {
+        featureType: 'water',
+        elementType: 'geometry',
+        stylers: [{ color: '#17263c' }]
+      },
+      {
+        featureType: 'water',
+        elementType: 'labels.text.fill',
+        stylers: [{ color: '#515c6d' }]
+      },
+      {
+        featureType: 'water',
+        elementType: 'labels.text.stroke',
+        stylers: [{ color: '#17263c' }]
+      }
+    ]
+  });
+}
+
+// ========================================================================= //
+//  //ACTION CLICK ADD
+// ========================================================================= //
+
+var buyt = [
+  { maxb: 'VN-1', tenxb: 'Limouse-32', from: 'HCM', to: 'Hà Nội', tendoanhnghiep: 'Phương Trang', trangthai: 'Dang hoat dong' },
+  { maxb: 'VN-2', tenxb: 'Limouse-31', from: 'HCM', to: 'Nha Trang', tendoanhnghiep: 'Phương Trang', trangthai: 'Khong hoat dong' }
+];
+
+render();
+
+var addBtn = document.querySelector('.btn-add');
+addBtn.addEventListener("click", addItem);
+
+function addItem() {
+  let idInput = document.getElementById('mabuyt');
+  let nameOfBusInput = document.getElementById('nameOfBus');
+  let fromInput = document.getElementById('from');
+  let toInput = document.getElementById('to');
+  let nameOfBusinessInput = document.getElementById('nameOfBusiness');
+  let trangThaiOfBus = document.getElementById('trangThai');
+
+  const newItem = {
+    maxb: idInput.value,
+    tenxb: nameOfBusInput.value,
+    from: fromInput.value,
+    to: toInput.value,
+    tendoanhnghiep: nameOfBusinessInput.value,
+    trangthai: trangThaiOfBus.value
+  };
+
+  buyt.push(newItem);
+  // buyt = [...buyt, newItem]
+  sessionStorage.setItem('buyt', JSON.stringify(buyt));
+  render(true);
+}
+
+
+function render(isRemove) {
+  let tableBody = document.querySelector('.table-bordered tbody');
+
+  if (isRemove) {
+    tableBody.innerHTML = '';
   }
+
+  const buytStore = JSON.parse(sessionStorage.getItem('buyt')) ?? buyt;
+
+  buytStore.forEach((item, index) => {
+    let row = document.createElement('tr');
+    row.innerHTML = `
+      <td>${item.maxb}</td>
+      <td>${item.tenxb}</td>
+      <td>${item.from} - ${item.to}</td>
+      <td>${item.tendoanhnghiep}</td>
+      <td>${item.trangthai}</td>
+      <td><button class="btn btn-warning btn-edit" onclick="editItem(${item.maxb})">Edit</button></td>
+      <td><button class="btn btn-danger btn-delete" onclick="deleteItem(${index})">Delete</button></td>
+    `;
+    tableBody.appendChild(row);
+  });
+}
+
+// ========================================================================= //
+//  //delete CLICK find
+// ========================================================================= //
+var findBtn = document.querySelector('btn-find');
+
+
+
+
